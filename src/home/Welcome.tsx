1704fc46c08c52bf6998276534c0ba0338e722de
alexkatz <alexanderikatz@gmail.com>
2017-11-02 00:48:00 -0400

    removed console log

---
+++ src/home/Welcome.tsx
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import { Style } from '../shared/styles';
+import { Color } from '../shared/colors';
+import { Constant } from '../shared/constants';
+
+const Welcome: React.StatelessComponent<{ width: number }> = ({ width }) => (
+    <div
+        style={{
+            width: '100%',
+            height: '100%',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            flexDirection: 'column',
+            ...Style.NO_SELECT,
+        }}
+    >
+        <div
+            style={{
+                flex: '25%',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                fontSize: Math.min(Math.round(width / 10), 70),
+                color: Color.MID_BLUE,
+                cursor: 'default',
+            }}
+        >
+            TimeStretcher
+        </div>
+        <div
+            style={{
+                flex: '75%',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                fontSize: Math.min(Math.round(width / 20), 50),
+                fontWeight: Constant.FontWeight.LIGHT,
+                color: Color.MID_BLUE,
+                cursor: 'default',
+            }}
+        >
+            Click anywhere, or drag an audio file in
+        </div>
+    </div>
+);
+
+export { Welcome };
---
+++ src/shared/constants.ts
@@ -0,0 +1,14 @@
+export const Constant = {
+    FontWeight: {
+        LIGHT: 300 as 300,
+        REGULAR: 400 as 400,
+    },
+    PADDING: 8,
+    Key: {
+        SPACE: 32,
+        SHIFT: 16,
+        Z: 90,
+        ESCAPE: 27,
+        S: 83,
+    },
+};
